e7f736ffaceb503b1ade50e004387e53704da570
Anil Reddy Saripiralla <anilsakr@users.noreply.github.com>
2023-03-15 20:44:09 +0530

    Accept Prompt Input to send to OpenAI

--- python-openai.py
+++ python-openai.py
@@ -1,9 +1,12 @@
 import requests
+import argparse
 
-
+parser = argparse.ArgumentParser()
+parser.add_argument("prompt", help="prompt to send the OpenAI API")
+args = parser.parse_args()
 
 api_endpoint = "https://api.openai.com/v1/completions"
-api_key = "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"
+api_key = "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"
 
 headers = {
     "Content-Type" : "application/json",
@@ -12,13 +15,19 @@ headers = {
 
 request_data = {
     "model": "text-davinci-003",
-    "prompt": "write python script for printing out todays date",
+    "prompt": f"write python script to {args.prompt}.",
     "max_tokens": 100,
     "temperature": 0.5
 }
+
 response = requests.post(api_endpoint, headers=headers, json=request_data)
 
 if response.status_code == 200:
     print(response.json()["choices"][0]["text"])
 else:
-    print(f"request failed: {str(response.status_code)} ")
+    print(f"request failed: {str(response.status_code)}")
+
+    
+    
+    
+# python3 py-chatgpt.py "print todays date"
