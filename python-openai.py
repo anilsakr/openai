d336abb0c38e987f1c13908e5b71ea8bf48e11c3
Anil Reddy Saripiralla <anilsakr@users.noreply.github.com>
2023-03-16 12:46:11 +0530

    file to save the py script

--- python-openai.py
+++ python-openai.py
@@ -3,10 +3,11 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("prompt", help="prompt to send the OpenAI API")
+parser.add_argument("file_name", help="name of the file to save the py script")
 args = parser.parse_args()
 
 api_endpoint = "https://api.openai.com/v1/completions"
-api_key = "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"
+api_key = "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"
 
 headers = {
     "Content-Type" : "application/json",
@@ -23,11 +24,11 @@ request_data = {
 response = requests.post(api_endpoint, headers=headers, json=request_data)
 
 if response.status_code == 200:
-    print(response.json()["choices"][0]["text"])
+    response_text = response.json()["choices"][0]["text"]
+    with open(args.file_name, "w") as file:
+        file.write(response_text)
 else:
     print(f"request failed: {str(response.status_code)}")
 
-    
-    
-    
-# python3 py-chatgpt.py "print todays date"
+
+# python3 py-chatgpt.py "print todays date" "print_date.py"
